4ef6fccf7d19b11a36105540b5490dbb6e588598
soufiane elg <145681688+palasse2@users.noreply.github.com>
2026-04-30 15:46:45 +0100

    Update translations.js

--- proper-parsec/public/translations.js
+++ proper-parsec/public/translations.js
@@ -2335,6 +2335,9 @@ const translations = {
       "https://res.cloudinary.com/dzidoxclo/image/upload/v1777558825/Screenshot_2026-04-30_150304_tl8auj.png",
       "https://res.cloudinary.com/dzidoxclo/image/upload/v1777558826/Screenshot_2026-04-30_150527_ca5j6j.png",
       "https://res.cloudinary.com/dzidoxclo/image/upload/v1777558826/Screenshot_2026-04-30_150346_b2mzd8.png"
+       "https://res.cloudinary.com/dzidoxclo/image/upload/v1777558825/Screenshot_2026-04-30_150633_u8qfzc.png",
+      "https://res.cloudinary.com/dzidoxclo/image/upload/v1777558825/Screenshot_2026-04-30_150659_f3weew.png",
+      "https://res.cloudinary.com/dzidoxclo/image/upload/v1777558826/Screenshot_2026-04-30_150435_cqwlej.png"
     ],
     fr: [
       "https://res.cloudinary.com/dzidoxclo/image/upload/f_auto,q_auto,w_600/v1771034226/Screenshot_2026-02-14_023015_xgi44w.png",
